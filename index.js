8041464346067b7638d3e430f63fbf23b70a48f2
masaka <maru88dev@gmail.com>
2020-05-02 10:17:54 +0900

    add express-basic-auth

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const path = require("path");
+const basicAuth = require("express-basic-auth");
+
+const user = process.env.AUTH_USER;
+const pass = process.env.AUTH_PASS;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+if (user && pass) {
+  app.use(
+    basicAuth({
+      unauthorizedResponse: () => {
+        return "Unauthorized";
+      },
+      challenge: true,
+      authorizer: (inputUser, inputPass) => {
+        return (
+          basicAuth.safeCompare(inputUser, user) &
+          basicAuth.safeCompare(inputPass, pass)
+        );
+      },
+    })
+  );
+}
 
 app.use(express.static(path.join(__dirname, "public")));
 
